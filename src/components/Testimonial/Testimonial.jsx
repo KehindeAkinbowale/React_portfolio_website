5230705b3444bd2d7e6f4d40a622212bebcba97b
KehindeAkinbowale <Kennyakinbowale@gmail.com>
2025-05-13 02:31:26 -0700

    Fourth commit

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Data } from './Data'
-import './testimonial.css'
+import './Testimonial.css'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
